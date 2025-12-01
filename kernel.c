1c370a34c41cec643af1081ee3c487a4e6496830
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-12-01 22:54:01 +0000

    Add files via upload

---
+++ kernel.c
@@ -0,0 +1,28 @@
+#include "base.h"
+
+void kernel_main()
+{
+        int n=0;
+        int xx;
+        char c[2000];
+        char *cc=c;
+        char Yellow=14;
+        int i=640/2;
+        int ii=0;
+        
+        char Black=0;
+	cls();
+        gputs(0,10," max. memory Mb.....");
+        xx=freesm();
+        int_to_string(xx,cc);
+        gputs(10,20,cc);
+        redraw();
+        while(1){}
+
+        
+        
+	
+	
+}
+
+
